a8b54d7643631bfdfa87a8cd89bdf63eaadaf3a3
Diego Pino Garcia <dpino@igalia.com>
2017-09-19 17:41:19 +0200

    Make test pass/fail in a dedicated worker

--- atomics/wait/web-worker-agent/workers/nan-timeout.js
+++ atomics/wait/web-worker-agent/workers/nan-timeout.js
@@ -2,9 +2,7 @@ importScripts('/resources/testharness.js');
 
 onmessage = function(e) {
   let sab = e.data[0];
-  assert_true(false);
   assert_equals(sab[0], 0);
   let ret = Atomics.wait(sab, 0, 0);
-  assert_equals(ret, "ok");
   postMessage([ret]);
 }
